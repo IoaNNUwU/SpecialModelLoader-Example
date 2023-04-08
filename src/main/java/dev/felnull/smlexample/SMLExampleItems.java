0dd3aa58d0bb56050c9344604273646fe699e81d
MORIMORI0317 <morimori.0317@outlook.jp>
2023-04-08 15:19:27 +0900

    CustomModelData exsample

--- src/main/java/dev/felnull/smlexample/SMLExampleItems.java
+++ src/main/java/dev/felnull/smlexample/SMLExampleItems.java
@@ -7,19 +7,28 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class SMLExampleItems {
     public static final Item OBJ_MODEL_ITEM = new Item(new FabricItemSettings());
     public static final Item OBJ_MODEL_ITEM2 = new Item(new FabricItemSettings());
+    public static final Item CUSTOM_MODEL_DATA_OBJ_MODEL_ITEM = new Item(new FabricItemSettings());
 
     public static void init() {
         register("obj_model_item", OBJ_MODEL_ITEM);
         register("obj_model_item2", OBJ_MODEL_ITEM2);
+        register("custom_model_data_obj_model_item", CUSTOM_MODEL_DATA_OBJ_MODEL_ITEM);
 
         ItemGroupEvents.MODIFY_ENTRIES_ALL.register((group, entries) -> {
             if (group == CreativeModeTabs.BUILDING_BLOCKS) {
                 entries.accept(OBJ_MODEL_ITEM);
                 entries.accept(OBJ_MODEL_ITEM2);
+
+                entries.accept(CUSTOM_MODEL_DATA_OBJ_MODEL_ITEM);
+
+                ItemStack customModelDataItem = new ItemStack(CUSTOM_MODEL_DATA_OBJ_MODEL_ITEM);
+                customModelDataItem.getOrCreateTag().putInt("CustomModelData", 1);
+                entries.accept(customModelDataItem);
             }
         });
     }
